02b65bdf464b45087cb4b079ded655496c0ba0b3
Chaisrithip <Waranchari@users.noreply.huggingface.co>
2023-09-06 10:20:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def main():
     st.title("Milk Quality Analysis")
 
     with st.form("questionaire"):
-        pH = st.slider("pH Value", min_value=0, max_value=14, step=0.1)
+        pH = st.slider("pH Value", min_value=0.0, max_value=14.0, step=0.1)
         Temperature = st.slider("Temperature", min_value=0, max_value=100)
         Taste = st.selectbox("Taste", ["Good","Bad"])
         Odor = st.selectbox("Odor", unique_values["Odor"])
